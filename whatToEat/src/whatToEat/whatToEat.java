ad9fafa88255b90f12a858db80366bea831250a0
Robert Malecki <robmalec@umich.edu>
2019-08-15 11:56:52 -0400

    Add files via upload

--- whatToEat/src/whatToEat/whatToEat.java
+++ whatToEat/src/whatToEat/whatToEat.java
@@ -3,63 +3,53 @@ package whatToEat;
 import java.util.Scanner;
 
 public class whatToEat {
-	
-	@SuppressWarnings({ "unused"})
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		char eventType = ' ';
 		int partySize = 0;
 		String result = "";
 		Scanner scn = new Scanner(System.in);
-		
+
 		try {
-			eventType = scn.next().toCharArray()[0];
+			eventType = scn.next().charAt(0);
 			partySize = scn.nextInt();
-			
+
 			result += "Since you're hosting a ";
-			switch (eventType)
-			{
+			switch (eventType) {
 			case 'c':
-				result += "casual "+ printNumParticipants(partySize) + "sandwiches ";
+				result += "casual " + printNumParticipants(partySize) + "sandwiches ";
 				break;
 			case 's':
-				result += "semi-formal "+ printNumParticipants(partySize) + "fried chicken ";
+				result += "semi-formal " + printNumParticipants(partySize) + "fried chicken ";
 				break;
 			case 'f':
-				result += "formal "+ printNumParticipants(partySize) + "chicken parmesan ";
+				result += "formal " + printNumParticipants(partySize) + "chicken parmesan ";
 				break;
 			default:
 				System.out.println("Invalid event type entered");
 				break;
 			}
-			
+
 			result += "prepared ";
-			
-			if (partySize <= 1)
-			{
+
+			if (partySize <= 1) {
 				result += "in the microwave";
-			}
-			else if (partySize <= 12 && partySize >= 2)
-			{
+			} else if (partySize <= 12 && partySize >= 2) {
 				result += "in your kitchen";
-			}
-			else if (partySize >= 13)
-			{
+			} else if (partySize >= 13) {
 				result += "by a caterer";
 			}
-			
+
 			result += ".";
-			
+
 			System.out.println(result);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
 		scn.close();
 	}
-	public static String printNumParticipants(int partySize)
-	{
+
+	public static String printNumParticipants(int partySize) {
 		if (partySize == 1) {
 			return "event for 1 participant, you should serve ";
 		}
